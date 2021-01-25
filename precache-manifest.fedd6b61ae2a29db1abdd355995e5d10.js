a4a52611894bc66670469197d583b99aec1ff952
ajayupreti <ajayupreti20194@gmail.com>
2021-01-25 20:42:24 +0530

    Updates

---
+++ precache-manifest.fedd6b61ae2a29db1abdd355995e5d10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d994ebec098cb5d6fd3be6aef2430d2",
+    "url": "/ajay-portfolio-react/index.html"
+  },
+  {
+    "revision": "8811f57c4caa9fc6c6c5",
+    "url": "/ajay-portfolio-react/static/css/main.b73741cb.chunk.css"
+  },
+  {
+    "revision": "7248f334ac9417bd4cbe",
+    "url": "/ajay-portfolio-react/static/js/2.47ab64fc.chunk.js"
+  },
+  {
+    "revision": "a1921694f88d68b5e5f6260c9f21d92f",
+    "url": "/ajay-portfolio-react/static/js/2.47ab64fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8811f57c4caa9fc6c6c5",
+    "url": "/ajay-portfolio-react/static/js/main.b9dacaca.chunk.js"
+  },
+  {
+    "revision": "03088425aec3a944fe17",
+    "url": "/ajay-portfolio-react/static/js/runtime-main.68b00220.js"
+  },
+  {
+    "revision": "0f2506edbc885fe42dc807043602cb94",
+    "url": "/ajay-portfolio-react/static/media/ajay_pic.0f2506ed.JPG"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ajay-portfolio-react/precache-manifest.fedd6b61ae2a29db1abdd355995e5d10.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ajay-portfolio-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
